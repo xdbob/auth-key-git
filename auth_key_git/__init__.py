2e7b0a24a04943bd888a5dcb08a05a4df3c78913
Antoine Damhet <antoine.damhet@lse.epita.fr>
2017-11-13 21:23:19 +0100

    Options can now be loaded from a config file
    Signed-off-by: Antoine Damhet <antoine.damhet@lse.epita.fr>

--- auth_key_git/__init__.py
+++ auth_key_git/__init__.py
@@ -1,54 +1,62 @@
 #!/usr/bin/env python3
 
+import configparser
 import ldap
+import os
 import sys
 from os.path import expanduser
 
-ldap_addr = 'ldaps://auth.pie.cri.epita.net'
-gitolite_user_shell = '/srv/git/bin/gitolite-shell'
-dn_group = 'ou=lse,ou=labos,dc=epita,dc=net'
+
+def load_config(filename):
+    defaults = {
+        'shell': '/usr/lib/gitolite/gitolite-shell',
+        'options': 'no-port-forwarding,no-X11-forwarding,no-agent-forwarding,no-pty',
+        'user': 'git',
+    }
+    config_files = [
+        '/etc/authkeygit/authkeygitrc',
+        expanduser('~/.authkeygitrc'),
+        'authkeygitrc',
+    ]
+    conf = configparser.SafeConfigParser(defaults)
+    conf.add_section('git')
+    conf.add_section('ldap')
+    conf.read(config_files)
+    return conf
 
 def main():
     if len(sys.argv) < 2:
-      exit(0)
+        exit(0)
     username = sys.argv[1]
 
+    config = load_config('/etc/auth-key-git.conf')
+    if username != config.get('git', 'user'):
+        exit(0)
+
+    def command(user):
+        return "command=\"%s %s\"" % (config.get('git', 'shell'), user)
+
+    def ssh_options(user, option):
+        return '{},{}'.format(command(user), option)
+
     try:
-      if username == 'git':
-        options = [
-          'no-port-forwarding',
-          'no-X11-forwarding',
-          'no-agent-forwarding',
-          'no-pty'
-        ]
-
-        def command(user):
-          return "command=\"%s %s\"" % (gitolite_user_shell, user)
-
-        def ssh_options(user, option):
-          return ','.join([command(user)] + option)
-
-        con = ldap.initialize(ldap_addr)
+        con = ldap.initialize(config.get('ldap', 'ldap_addr'))
         con.deref = ldap.DEREF_ALWAYS
 
-        filter = 'uid=*'
-        attrs = ['uid', 'sshPublicKey']
-        users = con.search_s(dn_group, ldap.SCOPE_SUBTREE, filter, attrs)
+        attrs = config.get('ldap', 'attrs').split(",")
+        users = con.search_s(config.get('ldap', 'dn_group'),
+                           ldap.SCOPE_SUBTREE,
+                           config.get('ldap', 'filter'),
+                           attrs)
 
         for user in users:
-          if 'sshPublicKey' in user[1]:
-            for key in user[1]['sshPublicKey']:
-              print(ssh_options(user[1]['uid'][0].decode("ascii"), options), key.strip().decode("ascii"))
-
-      try:
-        # XXX: sanity check missing, username exists
-        with open(expanduser('~%s/.ssh/authorized_keys' % username)) as f:
-          print(f.read()),
-      except:
-        pass
-
+            if 'sshPublicKey' in user[1]:
+                for key in user[1]['sshPublicKey']:
+                    options = ssh_options(user[1]['uid'][0].decode("ascii"), config.get('git', 'options'))
+                    print(options, key.strip().decode("ascii"))
     except:
         pass
+
 
 if __name__ == "__main__":
     main()
